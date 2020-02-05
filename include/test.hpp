430db8a109ab21373f0c0f861d97e60a0e85b0d0
jafri <syed.jafri2@ucalgary.ca>
2020-02-05 04:04:39 -0700

    checksum160

--- include/test.hpp
+++ include/test.hpp
@@ -13,18 +13,18 @@ CONTRACT test : public contract {
       : contract(receiver, code, ds),
         _hashes(receiver, receiver.value) {}
 
-    ACTION create ( const checksum512& hash );
+    ACTION create ( const checksum160& hash );
 
     TABLE Hashes {
       uint64_t index;
-      checksum512 hash;
+      checksum160 hash;
       uint64_t primary_key() const { return index; };
-      eosio::checksum512 by_hash() const { return hash; };
+      eosio::checksum160 by_hash() const { return hash; };
 
       EOSLIB_SERIALIZE(Hashes, (index)(hash));
     };
     typedef multi_index<"hashes"_n, Hashes,
-      indexed_by<name("byhash"), const_mem_fun<Hashes, checksum512, &Hashes::by_hash>>
+      indexed_by<name("byhash"), const_mem_fun<Hashes, checksum160, &Hashes::by_hash>>
     > hash_table;
     hash_table _hashes;
 };
