5e32df764594983eb73a2913a2c5948309755ced
jafri <syed.jafri2@ucalgary.ca>
2020-02-05 03:52:07 -0700

    checksum512

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,6 @@
 #include <test.hpp>
 
-void test::create ( const eosio::checksum160& hash ) {
+void test::create ( const eosio::checksum512& hash ) {
     auto hashed_index = _hashes.get_index<eosio::name("byhash")>();
     auto existing_hash = hashed_index.find(hash);
     check(existing_hash != hashed_index.end(), "hash does not exist.");
